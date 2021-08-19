9b7d38ce59c7e1667212084546f67c0f8a4fef40
Tegan Cadney-Gray <tegan.cadney@gmail.com>
2021-08-19 14:39:02 +0100

    validations added

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie_id, :list_id, presence: true
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
+end
